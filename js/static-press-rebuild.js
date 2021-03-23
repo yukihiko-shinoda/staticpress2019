f871e0d82ab77bb167ead2a8d14cb327460fcfb9
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2021-03-24 00:53:41 +0900

    Refactor to page object pattern

--- js/static-press-rebuild.js
+++ js/static-press-rebuild.js
@@ -3,19 +3,44 @@ jQuery(function ($) {
     this.path_ajax_loader_gif = $("#path-ajax-loader-gif").text();
     this.dom = $(
       '<div id="loader" style="line-height: 115px; text-align: center;"><img alt="activity indicator" src="' +
-        path_ajax_loader_gif +
+        this.path_ajax_loader_gif +
         '"></div>'
     );
     this.append_after = function (dom) {
       dom.after(this.dom);
     };
     this.remove = function () {
-      $("#loader").remove();
+      this.dom.remove();
+    };
+  }
+
+  function ResultList(dom) {
+    this.file_count = 0;
+    this.dom = $('<ul class="result-list"></ul>');
+    var dom_p = $('<p class="result-list-wrap"></p>');
+    dom_p.append(this.dom)
+    dom.append(dom_p)
+    this.display_file = function (files) {
+      var ul = this.dom;
+      var file_count = this.file_count;
+      $.each(files, function () {
+        if (this.static) {
+          file_count++;
+          ul.append("<li>" + file_count + " : " + this.static + "</li>");
+        }
+      });
+      this.dom = ul
+      this.file_count = file_count;
+      if (!($("li:last-child", this.dom).offset() === void 0)) {
+        $("html,body").animate(
+          { scrollTop: $("li:last-child", this.dom).offset().top },
+          "slow"
+        );
+      }
     };
   }
 
   function RebuildResult() {
-    this.file_count = 0;
     this.dom = $("#rebuild-result");
     this.text_error = $("#text-error").text();
     this.text_initialize = $("#text-initialize").text();
@@ -23,11 +48,13 @@ jQuery(function ($) {
     this.text_end = $("#text-end").text();
     this.text_urls = $("#text-urls").text();
     this.loader = new Loader();
+    this.result_list = null;
     this.display_message = function (text) {
       this.dom.append('<p id="message"><strong>' + text + "</strong></p>");
       $("html,body").animate({ scrollTop: $("#message").offset().top }, "slow");
     };
     this.display_error = function () {
+      this.loader.remove();
       this.display_message(this.text_error);
     };
     this.initialize = function () {
@@ -37,31 +64,16 @@ jQuery(function ($) {
     };
     this.display_fetch_start = function () {
       this.dom.append("<p><strong>" + this.text_fetch_start + "</strong></p>");
-      this.dom.append(
-        '<p class="result-list-wrap"><ul class="result-list"></ul></p>'
-      );
+      this.result_list = new ResultList(this.dom);
     };
     this.display_file = function (files) {
-      var ul = $("#rebuild-result ul.result-list");
-      var file_count = this.file_count;
-      $.each(files, function () {
-        if (this.static) {
-          file_count++;
-          ul.append("<li>" + file_count + " : " + this.static + "</li>");
-        }
-      });
-      this.file_count = file_count;
-      if (!($("li:last-child", ul).offset() === void 0)) {
-        $("html,body").animate(
-          { scrollTop: $("li:last-child", ul).offset().top },
-          "slow"
-        );
-      }
+      this.result_list.display_file(files);
     };
     this.display_end = function () {
+      this.loader.remove();
       this.display_message(this.text_end);
     };
-    this.append_empty_list = function (urls_count) {
+    this.append_number_url_per_type = function (urls_count) {
       this.dom.append("<p><strong>" + this.text_urls + "</strong></p>");
       var ul = $("<ul></ul>");
       $.each(urls_count, function () {
@@ -69,19 +81,10 @@ jQuery(function ($) {
       });
       this.dom.append("<p></p>").append(ul);
     };
-    this.scroll_to_message = function () {
-      $("html,body").animate({ scrollTop: $("#message").offset().top }, "slow");
-    };
     this.remove_loader = function () {
       this.loader.remove();
     };
-    this.reset_file_count = function () {
-      this.file_count = 0;
-    };
   }
-
-  var rebuild_result = new RebuildResult();
-  var admin_ajax_php = $("#admin-ajax-php").text();
 
   function DebugLogger() {
     const DEBUG_MODE_TRUE = "1";
@@ -93,16 +96,16 @@ jQuery(function ($) {
     };
   }
 
+  var admin_ajax_php = $("#admin-ajax-php").text();
+  var rebuild_result = new RebuildResult();
   var debug_logger = new DebugLogger();
 
   function error() {
     $("#rebuild").show();
-    rebuild_result.remove_loader();
     rebuild_result.display_error();
   }
 
   function static_press_init() {
-    rebuild_result.reset_file_count();
     $("#rebuild").hide();
     rebuild_result.initialize();
     $.ajax(admin_ajax_php, {
@@ -113,7 +116,7 @@ jQuery(function ($) {
       success: function (response) {
         debug_logger.log(response);
         if (response.result) {
-          rebuild_result.append_empty_list(response.urls_count);
+          rebuild_result.append_number_url_per_type(response.urls_count);
         }
         rebuild_result.display_fetch_start();
         static_press_fetch();
@@ -154,7 +157,6 @@ jQuery(function ($) {
       success: function (response) {
         debug_logger.log(response);
         $("#rebuild").show();
-        rebuild_result.remove_loader();
         rebuild_result.display_end();
       },
       error: error,
