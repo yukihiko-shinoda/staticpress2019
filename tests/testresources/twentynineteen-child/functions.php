277f4b19a1f35bc815f6cd99c16d2dea131189fd
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-11-22 16:45:41 +0900

    Fix test that files for test doesn't exist

---
+++ tests/testresources/twentynineteen-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Function for child theme.
+ * 
+ * @see https://developer.wordpress.org/themes/advanced-topics/child-themes/
+ * @package ?
+ */
+
+add_action( 'wp_enqueue_scripts', 'twentynineteen_child_my_theme_enqueue_styles' );
+/**
+ * Enqueues my theme's style.
+ * Reason: This file is belonging to not StaticPress but theme.
+ */
+function twentynineteen_child_my_theme_enqueue_styles() { // phpcs:ignore
+	$parent_style = 'twentynineteen-style';
+ 
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
+}
